a6004b6755cb0f2a12aa918bba59a172379a2d0c
VladimirMilanov <milanov_vladi@hotmail.com>
2017-04-21 13:06:14 +0300

    Adding file post/update_comment.php

---
+++ views/post/update_comment.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $model app\models\PostComment */
+
+?>
+<div class="comment-update">
+
+    <?= $this->render('_update_comment', [
+        'model' => $model,
+    ]) ?>
+
+</div>

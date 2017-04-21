0f9cad37469eba9b8925d93d9cde794ee1d815f7
VladimirMilanov <milanov_vladi@hotmail.com>
2017-04-21 14:00:25 +0300

    add file create-comment.php

---
+++ views/post/create-comment.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $model app\models\PostComment */
+
+?>
+<div class="create-comment">
+
+    <?= $this->render('_create', [
+        'model' => $model,
+    ]) ?>
+
+</div>
